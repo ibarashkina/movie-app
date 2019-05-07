f7bb46577a7fd34aa896ce43c7af9d5609b3e0a7
Ayaka <ayaka.hibino@gmail.com>
2019-05-07 19:55:39 -0400

    added a new component movieDetail to display more information for the user when they click on one of the movies from the movieList component

---
+++ src/app/movieDetail/movieDetail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'movie-detail',
+  templateUrl: './movieDetail.component.html',
+  styleUrls: ['./movieDetail.component.css']
+})
+export class MovieDetailComponent {
+}
